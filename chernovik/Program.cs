5a5ffad17f5ee160f674c927e7c3def1928c416f
Amir <amir_35@mail.ru>
2023-02-21 00:14:11 +0500

    Доп задача из телеги

---
+++ chernovik/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[10];
+Random random = new Random();
+int rnd = Convert.ToInt32(random.Next(100,1000));
+
+Console.WriteLine("Десятью случайно сгенерированными и интересными числами являются: ");
+for (int i = 0; i < 10; i++)
+    if (((rnd / 100) + ((rnd / 10) % 10) + (rnd % 10)) % 3 == 0)
+    {
+        array[i] = rnd;
+        rnd = Convert.ToInt32(random.Next(100,1000));
+        Console.Write($"{array[i]} \t");
+    }
+    else
+    {
+        i--;
+        rnd = Convert.ToInt32(random.Next(100,1000));
+    }
+
+
---
+++ chernovik2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int [] array = new int [10];
+Random rand = new Random();
+
+int numLen = array.Length;
+int temp = rand.Next(100,1000);
+
+for(int i = 0; i < numLen; i++)
+{
+      if ((temp / 100) + ((temp / 10) % 10) + (temp % 10) % 3 == 0)
+      {
+         array[i] = temp;
+      }
+}
+
+Console.WriteLine(temp);
+// Console.WriteLine(array);
+
+// Console.WriteLine(temp);
+// Console.WriteLine(temp / 100);
+// Console.WriteLine((temp / 10) % 10);
+// Console.WriteLine(temp % 10);
+
+// string[] test = new string[3];
+// Random rand = new Random();
+// for(int i =0;i<test.Length;i++)
+//    test[i] = Convert.ToString(rand.Next(-100,100));//Заполняем случайными числами диапозоном от -100 до 100
+
+// string result = string.Join(",", test);
+// Console.WriteLine(result);
